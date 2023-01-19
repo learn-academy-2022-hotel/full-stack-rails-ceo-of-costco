b5882043e8e65acdfc3487834751f3b29439d73b
oagomezz <ogomezbarragan437@gmail.com>
2023-01-19 11:34:04 -0800

    finsihed validations

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'all blog posts contain title' do
+    post = Blog.create content: 'more content'
+    expect(post.errors[:title]).to_not be_empty
+  end
+  it 'all blog posts contain content' do
+    post = Blog.create title: 'title'
+    expect(post.errors[:content]).to_not be_empty
+  end
+  it 'does not allow duplicate titles' do
+    Blog.create(title:'other title', content:'different content')
+    post2 =  Blog.create(title:'other title',content:'different content')
+    expect(post2.errors[:title]).to_not be_empty
+  end
+  it 'is not valid if title is less than 4 characters' do
+    post = Blog.create(title: 'tit', content: 'content more')
+    expect(post.errors[:title]).to_not be_empty
+  end
+  it 'is not valid if content is less than 10 characters' do
+    post = Blog.create(title: 'title', content: 'content')
+    expect(post.errors[:content]).to_not be_empty
+  end
+
 end
