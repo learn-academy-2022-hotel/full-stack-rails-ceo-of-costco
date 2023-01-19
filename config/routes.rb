daba1aa6cddec04e8aa31cfce9774eb1a7352ab3
oagomezz <ogomezbarragan437@gmail.com>
2023-01-19 11:01:03 -0800

    completed edit function

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'blogs' => 'blog#index', as: 'blogs'
   get 'blogs/new' => 'blog#new', as: 'new_blog'
   get 'blogs/:id' => 'blog#show', as: 'blog'
+  get 'blogs/:id/edit' => 'blog#edit', as: 'blog_edit'
   post 'blogs' => 'blog#create'
   delete '/blogs/:id' => 'blog#destroy', as: 'blog_destroy'
 
